0654162d42272109fa8992c0004239e003bd1537
UHa <https00200@gmail.com>
2023-10-17 17:12:45 +0900

    ✅ Add PostPage

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,13 @@
-export default function PostList() {
-    return <div>Post List</div>;
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import PostList from '../../components/PostList';
+
+export default function PostsPage() {
+    return (
+        <>
+            <Header />
+            <PostList hasNavigation={false} />
+            <Footer />
+        </>
+    );
 }
