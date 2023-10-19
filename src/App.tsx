0be095c829ef9bfa416521d0e17c93bcd06a72e9
UHa <https00200@gmail.com>
2023-10-19 19:08:05 +0900

    ✅ Update check login information in router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { app } from 'firebaseApp';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Router from 'components/Router';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     const auth = getAuth(app);
-    const [isAuthenticated, setIšAuthenticated] = useState<boolean>(!!auth?.currentUser);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
+            }
+        });
+    }, [auth]);
 
     return (
         <>
