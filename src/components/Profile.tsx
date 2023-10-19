15559ef1b118598bd201f3631e0d53c7f97a48b3
UHa <https00200@gmail.com>
2023-10-19 21:18:30 +0900

    ✅ Add Logout function

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,33 @@
-import { Link } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+import { app } from 'firebaseApp';
+import { toast } from 'react-toastify';
 
 export default function Profile() {
+    const auth = getAuth(app);
+
+    const onSignOut = async () => {
+        try {
+            await signOut(auth);
+            toast.success('로그아웃 되었습니다.');
+        } catch (err: any) {
+            console.log(err);
+            toast.error(err?.code);
+        }
+    };
+
     return (
         <>
             <div className="profile__box">
                 <div className="flex__box-lg">
                     <div className="profile__img" />
                     <div>
-                        <div className="profile__email">test@test.com</div>
-                        <div className="profile__name">사용자</div>
+                        <div className="profile__email">{auth?.currentUser?.email}</div>
+                        <div className="profile__name">{auth?.currentUser?.displayName || 'Guest'}</div>
                     </div>
                 </div>
-                <Link to="/" className="profile__logout">
+                <div role="presentation" onClick={onSignOut} className="profile__logout">
                     로그아웃
-                </Link>
+                </div>
             </div>
         </>
     );
