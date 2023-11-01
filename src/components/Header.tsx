8b262690f5b72d46c91a4d4d3d04787f9278084e
UHa <https00200@gmail.com>
2023-11-01 14:27:11 +0900

    🎨 Update by authenticated or not

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,41 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { app } from 'firebaseApp';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+    const auth = getAuth(app);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
+    const [init, setInit] = useState<boolean>(false);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
+            }
+            setInit(true);
+        });
+    }, [auth]);
+
     return (
         <header className="header">
             <Link to="/" className="header__logo">
                 ForFe
             </Link>
-            <div>
-                <Link to="/posts/create">글쓰기</Link>
-                <Link to="/posts">게시글</Link>
-                <Link to="/profile">프로필</Link>
-            </div>
+            {isAuthenticated ? (
+                <div>
+                    <Link to="/posts/create">글쓰기</Link>
+                    <Link to="/posts">게시글</Link>
+                    <Link to="/profile">프로필</Link>
+                </div>
+            ) : (
+                <div>
+                    <Link to="/login">로그인</Link>
+                    <Link to="/signup">회원가입</Link>
+                </div>
+            )}
         </header>
     );
 }
