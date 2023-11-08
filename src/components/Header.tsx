77848684815e9730431192b8dbd889004d604061
UHa <https00200@gmail.com>
2023-11-09 00:11:13 +0900

    💄 Modifying header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { app } from 'firebaseApp';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/LOGO.svg';
 
 export default function Header() {
     const auth = getAuth(app);
@@ -20,21 +21,26 @@ export default function Header() {
     }, [auth]);
 
     return (
-        <header className="header">
-            <Link to="/" className="header__logo">
-                ForFe
-            </Link>
-            {isAuthenticated ? (
-                <div>
-                    <Link to="/posts/create">글쓰기</Link>
-                    <Link to="/profile">프로필</Link>
+        <div className="header__container">
+            <header className="header">
+                <Link to="/" className="header__logo">
+                    <img src={logo} />
+                </Link>
+                <div className="header__nav">
+                    {isAuthenticated ? (
+                        <div>
+                            <Link to="/posts">게시글</Link>
+                            <Link to="/posts/create">글쓰기</Link>
+                            <Link to="/profile">프로필</Link>
+                        </div>
+                    ) : (
+                        <div>
+                            <Link to="/login">로그인</Link>
+                            <Link to="/signup">회원가입</Link>
+                        </div>
+                    )}
                 </div>
-            ) : (
-                <div>
-                    <Link to="/login">로그인</Link>
-                    <Link to="/signup">회원가입</Link>
-                </div>
-            )}
-        </header>
+            </header>
+        </div>
     );
 }
