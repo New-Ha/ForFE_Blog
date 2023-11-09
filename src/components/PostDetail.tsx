d2d46446e7e03f08e376052d2b76d242c8cd8f11
UHa <https00200@gmail.com>
2023-11-09 13:23:29 +0900

    💄 Modifying post-detail-page style

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -37,16 +37,15 @@ export default function PostDetail() {
         <>
             <div className="post__detail">
                 {post ? (
-                    <>
-                        <div className="post__box">
+                    <div className="post__detail-page">
+                        <div className="post__detail_box">
                             <div className="post__title">{post?.title}</div>
-                            <div className="post__profile-box">
-                                <div className="post__profile" />
-                                <div className="post__author-name">{post?.email}</div>
-                                <div className="post__date">{post?.createdAt}</div>
-                            </div>
-                            <div className="post__detail-utils-box">
-                                {post?.category && <div className="post__category">{post?.category}</div>}
+                            <div className="post__info">
+                                <div className="post__profile-box">
+                                    <div className="post__profile" />
+                                    <div className="post__author-name">{post?.email}</div>
+                                    <div className="post__date">{post?.createdAt}</div>
+                                </div>
                                 <div className="post__utils-btn-box">
                                     <Link to={`/posts/edit/${post?.id}`} className="post__edit">
                                         수정
@@ -56,13 +55,16 @@ export default function PostDetail() {
                                     </div>
                                 </div>
                             </div>
+                            <div className="post__detail-utils-box">
+                                {post?.category && <div className="post__category">{post?.category}</div>}
+                            </div>
                             <div className="post__contents post__text-pre-wrap">{post?.content}</div>
                             <div className="post__blogUrl">
                                 <a href={post?.blogUrl}>블로그로 이동</a>
                             </div>
                         </div>
                         <Comments post={post} getPost={getPost} />
-                    </>
+                    </div>
                 ) : (
                     <Loader />
                 )}
